095b836ede66a2d52aa07135372d94fbbed97ed3
CarlMcAteer <carlmc12@gmail.com>
2018-03-15 22:24:03 +0000

    Bed

---
+++ ex34.py
@@ -0,0 +1,19 @@
+# python
+# peacock
+# bear
+# kangaroo
+# whale
+# peacock
+# platypus
+# whale
+
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print animals[1]
+print animals[2]
+print animals[0]
+print animals[3]
+print animals[4]
+print animals[2]
+print animals[5]
+print animals[4]
---
+++ ex36.py
@@ -0,0 +1,14 @@
+def start():
+    # it was a dark and stormy night.
+
+def dead():
+    # Thsi is what it says when you die.
+
+def west():
+    # travel west into the cave. Find the magic rock.
+
+def east():
+    # travel east into the cave. Find the magic stick.
+
+def north():
+    # travel north into the cave. You need the magic wand.
