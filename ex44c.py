f03b8bec4e0e5e0be41bde612497631591cb693f
carlmcateer <carlmc12@gmail.com>
2018-04-10 15:02:31 +0100

    ex44

---
+++ ex44c.py
@@ -0,0 +1,17 @@
+class Parent(object):
+
+    def altered(self):
+        print "Parent altered()"
+
+class Child(Parent):
+
+    def altered(self):
+        print "Child, before Parent altered()"
+        super(Child, self).altered()
+        print "Child, after Patent altered()"
+
+dad = Parent()
+son = Child()
+
+dad.altered()
+son.altered()
