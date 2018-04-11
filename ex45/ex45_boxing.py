5b76e61e086c74d5733e99961b1c373ed98b448d
CarlMcAteer <carlmc12@gmail.com>
2018-04-11 17:56:02 +0100

    q

---
+++ ex45/ex45_boxing.py
@@ -0,0 +1,52 @@
+
+import random
+import time
+
+# Design a game where you are in a boxing match with an AI
+# It is a turn baed game where you have a fixed amount of stamina
+# When stamina drops to zero it is the other players turn
+#
+# Both players have a life bar, when you life is 0 the game is over.
+#
+# You have 3 offencive moves ranging from low damage, low stamina, reliable (jab)
+# to high damage, high stamina, unreliable (haymaker)
+#
+# Optional fetures:
+#
+# If you end your turn with stamina left over you have a chance to dodge attacks.
+#
+# In the opening menue you have the option to select more dificult chalanges(more live and stamina for enemy).
+#
+# You can use cheat codes to win the game.
+#
+# Cross and Haymaker must be "set up"
+
+
+class Intro(object):
+    # Description of the game and how you play.
+    pass
+
+class Boxer(objext):
+    def __init__(self, life, health):
+        pass
+
+    def jab():
+        pass
+
+    def cross():
+        pass
+
+    def haymaker():
+        pass
+
+class Player(Boxer):
+    pass
+
+class Enemey(Boxer):
+    pass
+
+class Engine(object):
+    pass
+
+class roundTimer():
+    pass
